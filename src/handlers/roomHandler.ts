1fd5b4257b98f605943fd1223b9c2cd5e3deb943
Mayur Prajapati <mproyur685@gmail.com>
2025-06-22 16:13:46 +0530

    logic for multiple joinee join in same room added

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -4,17 +4,22 @@ import { v4 as UUIDv4 } from "uuid";
 const roomHandler = (socket: Socket) => {
 
     const createRoom = () => {
+        //this will be our unique room id which multiple connection will exchange data
         const roomId = UUIDv4();
-        socket.join(roomId);
-        socket.emit("room-create", { roomId });
+        socket.join(roomId); // we will make the socket connection enter a new room
+        socket.emit("room-create", { roomId }); // we will emit an event from server side that socket connection has been added to a room 
         console.log("Room created with id", roomId);
     };
 
-    const joinedRoom = ({ roomId }: { roomId: string }) => {
+    // The below funtion is executed everytime a user(creator or joinee) joings a new room
+    const joinedRoom = ({ roomId, peerId }: { roomId: string, peerId: string }) => {
         
-        console.log("New user has joined room", roomId);
+        console.log("New user has joined room", roomId, "with peer id as",peerId);
     };
 
+    //when to call the above two function ?
+
+    // we will call the above two function when the client will emit events to create room and join room
     socket.on("create-room", createRoom);
     socket.on("joined-room", joinedRoom);
 };
